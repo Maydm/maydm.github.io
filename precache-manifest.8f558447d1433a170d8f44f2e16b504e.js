ae3e42d7b69b272e1f103d4a7b7b004ac461b12c
Ashlie <ashliecrooks@gmail.com>
2019-06-10 17:08:59 -0500

    Updates

---
+++ precache-manifest.8f558447d1433a170d8f44f2e16b504e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f45ddce3720de771923df44f35563d3",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "8d785f49fc0393d07638",
+    "url": "/react-todo/static/css/main.a349d779.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/react-todo/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "8d785f49fc0393d07638",
+    "url": "/react-todo/static/js/main.7ece3e56.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
